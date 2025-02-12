0bee2ed4a4a2a4d5131ad028e9c384138747e149
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2025-02-13 00:32:27 +0530

    hero section

---
+++ src/components/Phone.tsx
@@ -0,0 +1,44 @@
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import React, { HTMLAttributes } from "react";
+
+interface PhoneProps extends HTMLAttributes<HTMLDivElement> {
+  imgSrc: string;
+  dark?: boolean;
+}
+
+export default function Phone({
+  className,
+  imgSrc,
+  dark = false,
+  ...props
+}: PhoneProps) {
+  return (
+    <div
+      className={
+        (cn("relative pointer-events-none z-50 overflow-hidden"), className)
+      }
+      {...props}
+    >
+      <div className=" pointer-events-none z-50 select-none relative">
+        <img
+          fill
+          src={
+            dark
+              ? "/phone-template-dark-edges.png"
+              : "/phone-template-white-edges.png"
+          }
+          alt={imgSrc}
+        />
+        <div className=" absolute -z-10 inset-0">
+          <Image
+            fill
+            style={{ objectFit: "cover" }}
+            src={imgSrc}
+            alt="overlaying phone image"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
